ada2ff866f4f63a76921e4c6d5fb978d7811175e
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-01-15 13:02:40 -0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+"use client"
 import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
+  console.log("Hola mundo")
   return (
     <>
       <h1>Hola</h1>
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+"use client"
+import Link from "next/link"
+
+export const Header = () => {
+    return (
+        <>
+            <header>
+                <nav>
+                    <ul>
+                    <Link href="/">
+                        <li>Home</li>
+                    </Link>
+                    <Link href="/store">
+                        <li>Store</li>
+                    </Link>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    )
+}

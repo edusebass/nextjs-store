e182855c2c5604874d867f8de462ce77c33fb8f5
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-01-18 11:06:40 -0500

    route grrouping y listado de productos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Footer } from 'app/components/shared/Footer'
 import 'app/sass/globals.sass'
 
 
+
 const roboto = Roboto({
   weight: ["100", "300", "500", "700"],
   subsets: ["latin"]
@@ -24,6 +25,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={roboto.className}>
         <Header />
+
         {children}
         <Footer />
       </body>

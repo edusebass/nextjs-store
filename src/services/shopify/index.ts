05195fff727e5d76042c29b19a2c9db4d9f9e416
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-01-18 14:51:56 -0500

    estructuarizacion

--- src/services/shopify/index.ts
+++ src/services/shopify/index.ts
@@ -2,12 +2,15 @@ import { env } from "app/config/env"
 import { shopifyUrls } from "./urls"
 
 export const getProducts = async () => {
-    const response = await fetch(shopifyUrls.products.all, {
+    try {
+        const response = await fetch(shopifyUrls.products.all, {
         headers: new Headers({
             'X-Shopify-Access-Token': env.SHOPIFY_TOKEN
         })
-    })
-
-    const {products} = await response.json()
-    return products
+        })
+        const { products } = await response.json()
+        return products
+    } catch (error) {
+        console.log(error)
+    }
 }

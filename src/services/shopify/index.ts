a515d5fc5cbf1be9ae772342e0513e00d78ca088
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-01-18 13:53:23 -0500

    arquitectura de archivos escalada

---
+++ src/services/shopify/index.ts
@@ -0,0 +1,13 @@
+import { env } from "app/config/env"
+import { shopifyUrls } from "./urls"
+
+export const getProducts = async () => {
+    const response = await fetch(shopifyUrls.products.all, {
+        headers: new Headers({
+            'X-Shopify-Access-Token': env.SHOPIFY_TOKEN
+        })
+    })
+
+    const {products} = await response.json()
+    return products
+}

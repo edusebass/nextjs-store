abd37c1ec4b7e6ab846254b036f086dae4d09e58
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-01-18 10:01:49 -0500

    obtener porductos

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,4 +1,15 @@
-export const MainProducts = () => {
+const getProducts = async () => {
+    const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`, {
+        headers: new Headers({
+            'X-Shopify-Access-Token': process.env.SHOPIFY_ACESS_TOKEN || ""
+        })
+    })
+
+    const data = await response.json()
+    return console.log(data)
+}
+export const MainProducts = async () => {
+    const products = await getProducts()
     return(
         <>
             <section>

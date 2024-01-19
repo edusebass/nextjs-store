8b8aa380f09a464b79174b95ba1087a6ce79eb5a
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-01-19 16:39:14 -0500

    bug arreglado con filtros

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -13,11 +13,9 @@ export default async function Category(props: CategoryProps) {
     const { categories } = props.params;
     let products = await getProducts();
     const collections = await getCollections()
-    console.log(collections)
-    const selectedCollectionId = collections.find((collection) => collection.handle === categories[0]).id
-    console.log(selectedCollectionId)
-
-    if(selectedCollectionId){
+    
+    if(categories?.length > 0){
+        const selectedCollectionId = collections.find((collection: any) => collection.handle === categories[0]).id
         products = await getCollectionsProducts(selectedCollectionId)
     } else {
         products = await getProducts()

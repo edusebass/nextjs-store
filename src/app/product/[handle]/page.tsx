13d7a875862c69344f24acad41e5a66c217376ba
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-01-20 12:26:02 -0500

    vista individual del producto

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,12 +1,22 @@
-"use client"
-import { useParams, useSearchParams } from "next/navigation"
+import { ProductView } from "app/components/product/ProductView"
+import { getProducts } from "app/services/shopify/products"
 
-
-export default function ProductPage(){
-    const params = useParams()
-    const searchParams = useSearchParams()
-    const id = searchParams.get("id")
-
-    console.log('searchParams', id)
-    return <h1>Product Page</h1>
+interface ProductPageProps {
+  searchParams: {
+    id: string
+  }
 }
+
+export default async function ProductPage({ searchParams }: ProductPageProps){
+    const id = searchParams.id
+
+    const products = await getProducts(id)
+    const product = products[0]
+    console.log('searchParams', id)
+    return(
+      <>
+        <ProductView product={product}/>
+      </>
+    )
+}
+

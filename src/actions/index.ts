937a2c5e42845885a07f2c2198bdbe743bd056e6
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-01-22 12:58:24 -0500

    confirgaicion para graphsql

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,4 +1,8 @@
 "use server"
+
+import { GraphQLClientSingleton } from "app/graphql";
+
 export const handleCreateUser = (formData) => {
     console.log("handlecreateuser", formData);
+    const graphlClient = GraphQLClientSingleton.getInstance().getClient()
 }

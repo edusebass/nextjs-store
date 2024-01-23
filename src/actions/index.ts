2a9bb8a3a000a8b9d78fcc9e39982ab3cd04ffab
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-01-23 12:25:19 -0500

    creacion de usuario

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,8 +1,24 @@
 "use server"
 
 import { GraphQLClientSingleton } from "app/graphql";
+import { createUserMutation } from "app/graphql/mutations/createUserMutation";
 
-export const handleCreateUser = (formData) => {
+export const handleCreateUser = async (formData: FormData) => {
+    const formDataObject = Object.fromEntries(formData)
+    delete formDataObject["password_confirmation"]
+
     console.log("handlecreateuser", formData);
+
     const graphlClient = GraphQLClientSingleton.getInstance().getClient()
+    const variables = {
+        input:{
+            ...formDataObject,
+            phone: '+593' + formDataObject.phone
+        }
+    }
+
+    const { customerCreate } = await graphlClient.request(createUserMutation, variables)
+    const { customerUserErrors, customer } = customerCreate
+    console.log(customer)
+    console.log(customerUserErrors)
 }

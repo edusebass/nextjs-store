bac99b7a9f28b1bf12372744765fa1bf79c71fd7
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-01-24 11:18:57 -0500

    flujo de login

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,6 +2,8 @@
 
 import { GraphQLClientSingleton } from "app/graphql";
 import { createUserMutation } from "app/graphql/mutations/createUserMutation";
+import { createAcessToken } from "app/utils/auth/createAcessToken";
+import { redirect } from "next/navigation";
 
 export const handleCreateUser = async (formData: FormData) => {
     const formDataObject = Object.fromEntries(formData)
@@ -21,4 +23,17 @@ export const handleCreateUser = async (formData: FormData) => {
     const { customerUserErrors, customer } = customerCreate
     console.log(customer)
     console.log(customerUserErrors)
+
+    if(customer?.firstName){
+        await createAcessToken(formDataObject.email as string, formDataObject.password as string)
+        redirect("/store")
+    }
+}
+
+export const handleLogin = async (formData: FormData) =>{
+    const formDataObject = Object.fromEntries(formData)
+    const accesToken = await createAcessToken(formDataObject.email as string, formDataObject.password as string)
+    if(accesToken){
+        redirect("/store")
+    }
 }

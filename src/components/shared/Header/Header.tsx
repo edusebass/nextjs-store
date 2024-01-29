ec8a8b334117a614dd896905d4daf68f9e300c54
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-01-29 11:16:29 -0500

    update

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -4,26 +4,26 @@ import styles from './Header.module.sass'
 
 
 export const Header = async () => {
-  const customer = await validateAccessToken()
+    const customer = await validateAccessToken()
 
-  return (
+    return (
     <header className={styles.Header}>
-      <nav>
-        <ul className={styles.Header__list}>
-          <li>
-            <Link href="/">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link href="/store">
-              Store
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <div className={styles.Header__user}>
-        {customer?.firstName ? (<p>Hola! {customer.firstName}</p>) : (<Link href="/login">Login</Link>)}
-      </div>
+        <nav>
+            <ul className={styles.Header__list}>
+            <li>
+                <Link href="/">
+                Home
+                </Link>
+            </li>
+            <li>
+                <Link href="/store">
+                Store
+                </Link>
+            </li>
+            </ul>
+        </nav>
+        <div className={styles.Header__user}>
+            {customer?.firstName ? (<p>Hola! {customer.firstName}</p>) : (<Link href="/login">Login</Link>)}
+        </div>
     </header>)
 }
